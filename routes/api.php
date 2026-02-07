e09631e69a1ac798a36477707c7f86e166e77bdd
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-07 12:54:27 -0600

    feat: organize API routes by requiring necessary route files

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('hola', function () {
-    return 'hola mundo';
-});
+require __DIR__.'/api/public.php';
+require __DIR__.'/api/auth.php';
+require __DIR__.'/api/protected.php';
+require __DIR__.'/api/roles.php';
+require __DIR__.'/api/users.php';
+require __DIR__.'/api/security.php';
