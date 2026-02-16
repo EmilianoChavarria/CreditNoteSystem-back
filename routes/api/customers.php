18fa40a55636770861fd86b3801d2af01659fda2
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-16 14:52:08 -0600

    feat: add API routes for CustomerController with CRUD operations

---
+++ routes/api/customers.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\CustomerController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['jwt'])->group(function () {
+    Route::get('customers', [CustomerController::class, 'index']);
+    Route::get('customers/{id}', [CustomerController::class, 'show']);
+    Route::post('customers', [CustomerController::class, 'store']);
+    Route::put('customers/{id}', [CustomerController::class, 'update']);
+    Route::delete('customers/{id}', [CustomerController::class, 'destroy']);
+});
