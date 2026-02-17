b5851421272e9030add5f4183f6963717de16744
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-17 11:01:44 -0600

    feat: add rolesPermission and modules routes to API

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ require __DIR__.'/api/public.php';
 require __DIR__.'/api/auth.php';
 require __DIR__.'/api/protected.php';
 require __DIR__.'/api/roles.php';
+require __DIR__.'/api/rolesPermission.php';
+require __DIR__.'/api/modules.php';
 require __DIR__.'/api/users.php';
 require __DIR__.'/api/security.php';
 require __DIR__.'/api/customers.php';
