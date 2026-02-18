ef235faaca78ae14cb14839afa8a4d62e27c7272
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-18 10:52:53 -0600

    refactor: remove duplicate route definition for getting all requests

--- routes/api/requests.php
+++ routes/api/requests.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['jwt'])->group(function () {
+    Route::get('requests', [RequestController::class, 'getAll']);
     Route::get('requests/reasons', [RequestController::class, 'getAllReasons']);
     Route::post('requests/newRequest', [RequestController::class, 'createRequest']);
 });
