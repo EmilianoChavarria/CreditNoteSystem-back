3c77cc765f74b64016d59c3a0642b9294e5e3e80
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-02-18 10:51:20 -0600

    feat: create RequestClassification model with fillable attributes and relationships

---
+++ app/Models/RequestClassification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RequestClassification extends Model
+{
+    use HasFactory;
+
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+
+    protected $table = 'requestClassification';
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $casts = [
+        'createdAt' => 'datetime',
+        'updatedAt' => 'datetime',
+    ];
+
+    public function requests()
+    {
+        return $this->hasMany(Request::class, 'classificationId');
+    }
+
+    public function requestTypes()
+    {
+        return $this->belongsToMany(
+            RequestType::class,
+            'classificationtypes',
+            'classificationId',
+            'typeRequestId'
+        );
+    }
+}
