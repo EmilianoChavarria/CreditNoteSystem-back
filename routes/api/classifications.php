d4162e4080a2bca4ae3fb83d453687a24b1306c0
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-03 17:57:18 -0600

    feat: add getClassificationGrouped method to ClassificationController and corresponding route

--- routes/api/classifications.php
+++ routes/api/classifications.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt'])->group(function () {
     Route::post('classifications', [ClassificationController::class, 'store']);
+    Route::get('classifications/grouped', [ClassificationController::class, 'getClassificationGrouped']);
     Route::get('classifications/requestType/{typeRequestId}', [ClassificationController::class, 'getAllByRequestTypeId']);
 });
