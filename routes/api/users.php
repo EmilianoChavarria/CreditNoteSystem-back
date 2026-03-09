aa8c0c376022cd2ca0bcc0479c743e807d2f3b04
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-09 17:55:57 -0600

    feat: add route for retrieving users by sales and manager roles

--- routes/api/users.php
+++ routes/api/users.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['jwt'])->group(function () {
     Route::get('users', [UserController::class, 'getAll']);
     Route::get('usersPag', [UserController::class, 'index']);
+    Route::get('users/managers', [UserController::class, 'usersBySalesAndManagerRoles']);
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::post('users', [UserController::class, 'store']);
     Route::put('users/{id}', [UserController::class, 'update']);
