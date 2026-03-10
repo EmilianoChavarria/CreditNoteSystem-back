62325875d5cc1dc6a89dedd306b0b0e8796b4ea2
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-10 10:34:54 -0600

    feat: add route to retrieve all workflows with steps

--- routes/api/workflowSteps.php
+++ routes/api/workflowSteps.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt'])->group(function () {
     Route::get('workflowsteps', [WorkflowStepController::class, 'getAll']);
+    Route::get('workflowsteps/workflows', [WorkflowStepController::class, 'getAllWorkflowsWithSteps']);
     Route::get('workflowsteps/workflow/{workflowId}', [WorkflowStepController::class, 'getByWorkflowId']);
     Route::get('workflowsteps/{id}', [WorkflowStepController::class, 'getById']);
     Route::post('workflowsteps', [WorkflowStepController::class, 'store']);
