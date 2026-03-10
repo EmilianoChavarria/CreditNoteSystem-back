0b022faa649a30d4de77127fca1bd1054215df4b
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-10 10:34:42 -0600

    feat: enhance searchByName method to include additional client external table data

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -211,11 +211,14 @@ class CustomerController extends Controller
         }
 
         $searchTerm = $request->input('search');
-
-        $customerTable = (new Customer())->getTable();
         $clientTable = 'clientes_tme700618rc7';
+        $clientExtTable = 'clientes_tme700618rc7_ext';
+
         $clientColumns = Schema::hasTable($clientTable)
             ? Schema::getColumnListing($clientTable)
+            : [];
+        $clientExtColumns = Schema::hasTable($clientExtTable)
+            ? Schema::getColumnListing($clientExtTable)
             : [];
 
         $canReadClients = in_array('idCliente', $clientColumns, true)
@@ -233,82 +236,43 @@ class CustomerController extends Controller
             ), 201);
         }
 
-        $selectColumns = [
-            'c.idCustomer',
-            'c.idClient',
-            'c.salesEngineerId',
-            'c.salesManagerId',
-            'c.financeManagerId',
-            'c.marketingManagerId',
-            'c.customerServiceManagerId',
-            'c.area',
-            'se.id as salesEngineer_id',
-            'se.fullName as salesEngineer_name',
-            'sm.id as salesManager_id',
-            'sm.fullName as salesManager_name',
-            'fm.id as financeManager_id',
-            'fm.fullName as financeManager_name',
-            'mm.id as marketingManager_id',
-            'mm.fullName as marketingManager_name',
-            'csm.id as customerServiceManager_id',
-            'csm.fullName as customerServiceManager_name',
-        ];
+        $selectColumns = [];
 
         foreach ($clientColumns as $column) {
             $selectColumns[] = 'cl.' . $column . ' as client_' . $column;
         }
 
+        foreach ($clientExtColumns as $column) {
+            if ($column === 'idCliente') {
+                continue;
+            }
+
+            $selectColumns[] = 'cle.' . $column . ' as client_ext_' . $column;
+        }
+
         $customers = DB::table($clientTable . ' as cl')
-            ->leftJoin($customerTable . ' as c', 'c.idClient', '=', 'cl.idCliente')
-            ->leftJoin('users as se', 'c.salesEngineerId', '=', 'se.id')
-            ->leftJoin('users as sm', 'c.salesManagerId', '=', 'sm.id')
-            ->leftJoin('users as fm', 'c.financeManagerId', '=', 'fm.id')
-            ->leftJoin('users as mm', 'c.marketingManagerId', '=', 'mm.id')
-            ->leftJoin('users as csm', 'c.customerServiceManagerId', '=', 'csm.id')
-            ->whereNotNull('c.idCustomer')
+            ->leftJoin($clientExtTable . ' as cle', 'cle.idCliente', '=', 'cl.idCliente')
             ->where('cl.razonSocial', 'LIKE', '%' . $searchTerm . '%')
             ->orderBy('cl.idCliente')
             ->select($selectColumns)
             ->get()
-            ->map(function ($row) use ($clientColumns) {
+            ->map(function ($row) use ($clientColumns, $clientExtColumns) {
                 $clientData = [];
 
                 foreach ($clientColumns as $column) {
                     $clientData[$column] = $row->{'client_' . $column} ?? null;
                 }
 
-                $customerData = [
-                    'idCustomer' => $row->idCustomer,
-                    'idClient' => $row->idClient,
-                    'area' => $row->area,
-                    'salesEngineerId' => $row->salesEngineerId,
-                    'salesManagerId' => $row->salesManagerId,
-                    'financeManagerId' => $row->financeManagerId,
-                    'marketingManagerId' => $row->marketingManagerId,
-                    'customerServiceManagerId' => $row->customerServiceManagerId,
-                    'salesEngineer' => [
-                        'id' => $row->salesEngineer_id,
-                        'fullName' => $row->salesEngineer_name,
-                    ],
-                    'salesManager' => [
-                        'id' => $row->salesManager_id,
-                        'fullName' => $row->salesManager_name,
-                    ],
-                    'financeManager' => [
-                        'id' => $row->financeManager_id,
-                        'fullName' => $row->financeManager_name,
-                    ],
-                    'marketingManager' => [
-                        'id' => $row->marketingManager_id,
-                        'fullName' => $row->marketingManager_name,
-                    ],
-                    'customerServiceManager' => [
-                        'id' => $row->customerServiceManager_id,
-                        'fullName' => $row->customerServiceManager_name,
-                    ],
-                ];
+                $clientExtData = [];
+                foreach ($clientExtColumns as $column) {
+                    if ($column === 'idCliente') {
+                        continue;
+                    }
 
-                return array_merge($clientData, ['customer' => $customerData]);
+                    $clientExtData[$column] = $row->{'client_ext_' . $column} ?? null;
+                }
+
+                return array_merge($clientData, ['clienteExt' => $clientExtData]);
             })
             ->values();
 
