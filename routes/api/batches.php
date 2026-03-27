c78b399da2dcc90e781dcd9ab90f1f22f36788f9
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-27 16:51:57 -0600

    feat: add routes for retrieving batches and their requests

--- routes/api/batches.php
+++ routes/api/batches.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\BatchController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt'])->group(function () {
+    Route::get('batches', [BatchController::class, 'index']);
     Route::post('batches', [BatchController::class, 'store']);
     Route::get('batches/{id}', [BatchController::class, 'show']);
+    Route::get('batches/{id}/requests', [BatchController::class, 'requests']);
 });
