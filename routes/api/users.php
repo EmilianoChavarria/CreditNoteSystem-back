30a1607a584506837ea3dbb65a7b4aff31dc455a
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-03-31 16:14:50 -0600

    feat: add routes for user assignments in UserAssignmentController

--- routes/api/users.php
+++ routes/api/users.php
@@ -1,13 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\UserAssignmentController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt'])->group(function () {
     Route::get('users', [UserController::class, 'getAll']);
     Route::get('usersPag', [UserController::class, 'index']);
+    Route::get('users/assignment/leaders', [UserAssignmentController::class, 'leaders']);
+    Route::get('users/assignment/assignable-users', [UserAssignmentController::class, 'assignableUsers']);
     Route::get('users/managers', [UserController::class, 'usersBySalesAndManagerRoles']);
     Route::get('users/me', [UserController::class, 'me']);
+    Route::get('users/{leaderUserId}/assignments', [UserAssignmentController::class, 'index']);
+    Route::put('users/assignments', [UserAssignmentController::class, 'upsert']);
+    Route::delete('users/{leaderUserId}/assignments/{assignedUserId}', [UserAssignmentController::class, 'destroy']);
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::post('users', [UserController::class, 'store']);
     Route::put('users/{id}', [UserController::class, 'update']);
