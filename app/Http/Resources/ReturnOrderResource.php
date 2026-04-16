0ab760649a0da02bb948401fba71d9d60a99253e
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-16 17:15:25 -0600

    feat: add ReturnOrderResource for formatting return order data

---
+++ app/Http/Resources/ReturnOrderResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReturnOrderResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'        => $this->id,
+            'clientId'  => $this->clientId,
+            'userId'    => $this->userId,
+            'status'    => $this->status,
+            'notes'     => $this->notes,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+            'items'     => ReturnOrderItemResource::collection($this->whenLoaded('items')),
+        ];
+    }
+}
