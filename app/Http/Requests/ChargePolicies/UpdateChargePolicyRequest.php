4f3115eb54eba0fc0864ade885d00dbe9006dcc5
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-17 14:53:30 -0600

    feat: create UpdateChargePolicyRequest for validating charge policy updates

---
+++ app/Http/Requests/ChargePolicies/UpdateChargePolicyRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\ChargePolicies;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateChargePolicyRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'day'        => ['sometimes', 'integer', 'min:1'],
+            'percentage' => ['sometimes', 'numeric', 'min:0', 'max:100'],
+        ];
+    }
+}
