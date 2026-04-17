2fe4af7533c1658ac08d02c9185b5b10a7328260
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-17 14:53:36 -0600

    feat: create ChargePolicyResource for transforming charge policy data

---
+++ app/Http/Resources/ChargePolicyResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChargePolicyResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'day'        => $this->day,
+            'percentage' => $this->percentage,
+            'createdAt'  => $this->createdAt,
+            'updatedAt'  => $this->updatedAt,
+            'deletedAt'  => $this->deletedAt,
+        ];
+    }
+}
