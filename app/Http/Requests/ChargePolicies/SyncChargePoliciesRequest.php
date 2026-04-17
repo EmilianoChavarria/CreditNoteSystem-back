25d5a8feef3a0536632412383a1c282eb2ec5b12
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-04-17 14:53:25 -0600

    feat: create SyncChargePoliciesRequest for validating charge policy synchronization

---
+++ app/Http/Requests/ChargePolicies/SyncChargePoliciesRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\ChargePolicies;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SyncChargePoliciesRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'policies'              => ['required', 'array', 'min:1'],
+            'policies.*.id'         => ['nullable', 'integer', 'exists:chargePolicies,id'],
+            'policies.*.day'        => ['required', 'integer', 'min:1'],
+            'policies.*.percentage' => ['required', 'numeric', 'min:0', 'max:100'],
+        ];
+    }
+}
