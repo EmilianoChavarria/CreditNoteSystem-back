876a5650e02544d7f0ddb885d2ae8bf3273028fc
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-05-12 17:40:07 -0600

    feat(database): add MySQL configuration for invoices database connection

--- config/database.php
+++ config/database.php
@@ -63,6 +63,21 @@ return [
             ]) : [],
         ],
 
+        'invoices' => [
+            'driver'    => env('INVOICES_DB_DRIVER', 'mysql'),
+            'url'       => env('INVOICES_DB_URL'),
+            'host'      => env('INVOICES_DB_HOST', '127.0.0.1'),
+            'port'      => env('INVOICES_DB_PORT', '3306'),
+            'database'  => env('INVOICES_DB_DATABASE', ''),
+            'username'  => env('INVOICES_DB_USERNAME', 'root'),
+            'password'  => env('INVOICES_DB_PASSWORD', ''),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            'strict'    => true,
+            'engine'    => null,
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
