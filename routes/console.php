ed402468ada7fde2f956e3da3595d6a7148f81fb
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-06-17 17:57:18 -0600

    feat(schedule): add SendPendingApprovalReminders command and daily schedule

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Console\Commands\SendPendingApprovalReminders;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(SendPendingApprovalReminders::class)
+    ->dailyAt('15:56')
+    ->timezone('America/Mexico_City');
+
+
