9afd5ef538c44f9b2c1727e130c6dfd8d2ef510d
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-06-22 14:34:35 -0600

    feat(distributors): add distributors migration and model

---
+++ app/Models/Distributor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Distributor extends Model
+{
+    public const CREATED_AT = 'createdAt';
+    public const UPDATED_AT = 'updatedAt';
+
+    protected $table = 'distributors';
+
+    protected $fillable = [
+        'businessName',
+        'taxId',
+        'address',
+        'emails',
+        'clientNumber',
+    ];
+}
