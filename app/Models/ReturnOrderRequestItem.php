a70e46e59b52c51c4d007137d1e5c14657fd615e
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-07-08 17:09:13 -0600

    feat(db): add soft deletes to return order request items

--- app/Models/ReturnOrderRequestItem.php
+++ app/Models/ReturnOrderRequestItem.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReturnOrderRequestItem extends Model
 {
+    use SoftDeletes;
+
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
+    const DELETED_AT = 'deletedAt';
 
     protected $table = 'returnorderrequestitems';
 
