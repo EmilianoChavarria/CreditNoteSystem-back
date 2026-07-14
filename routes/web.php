ff6d03a99441ec5b247e000c99eeba8240c412cb
EmilianoChavarria <20223tn089@utez.edu.mx>
2026-07-14 15:11:15 -0600

    fix(routes): update Request facade import to use correct namespace

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
     return view('welcome');
